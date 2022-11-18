958087126ae90b0a0ee8405e74b6e1827c8d40eb
aprilkrg <aprilkrgonzales@gmail.com>
2022-11-18 10:29:24 -0700

    write plan for lab solve

---
+++ clock.js
@@ -0,0 +1,17 @@
+/*
+WHAT DO WE NEED TO STORE IN VARIABLES?
+- hour
+- minute
+- second
+^ need both a js variable and a html dom variable
+
+- function to rotate the arms
+^ need one per hand?
+
+HOW DO WE APPROACH?
+- create variables in js
+- write functions to console log values
+- check terminal to see if it's working
+- once it functions, then we'll move onto the dom
+
+*/
