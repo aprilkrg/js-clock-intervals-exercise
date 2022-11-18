1211caf61cb06b28ca71094dda0f324fe8ff1066
aprilkrg <aprilkrgonzales@gmail.com>
2022-11-18 10:36:43 -0700

    seconds increment in runClock function

--- clock.js
+++ clock.js
@@ -15,3 +15,20 @@ HOW DO WE APPROACH?
 - once it functions, then we'll move onto the dom
 
 */
+
+// variables to hold the time values
+let seconds = 0
+let minutes = 0
+let hours = 0
+
+// function to increment seconds
+function runClock() {
+    // increment seconds
+    seconds++
+    // console log seconds value to check if it incremented
+    console.log("sec:", seconds)
+}
+// invoke the funciton to see if it increments in the terminal
+runClock()
+// set the interval to run every second, and save to a variable
+const clockInterval = setInterval(runClock, 1000)
